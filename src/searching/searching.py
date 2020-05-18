bfb4ef9654644fa4aa5baa4ffc1bba0a580dcded
benjamin-solt <bensolt@gmail.com>
2020-05-18 15:07:45 -0400

    did binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -13,5 +13,21 @@ def binary_search(arr, target):
     left = 0
     right = len(arr) - 1
 
+    while left <= right:
+        # find midpoint. left plus right divide by 2.
+        mid = (left + right) // 2
+    
+    # is mid element equal to our target?
+        if arr[mid] == target:
+            #return result of mid
+            return mid
+        # mid element is less than our target
+        if arr[mid] < target:
+            # midd + 1
+            left = mid + 1
+        else:
+            #mid - 1
+            right = mid - 1
 
+#did not find element
     return -1  # not found
